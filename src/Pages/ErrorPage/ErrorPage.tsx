c52b2c5c9f90ac01f313a628bf34dfccf473dbe3
MuonNghiCode <minhquanpm1610@gmail.com>
2025-01-17 23:55:32 +0700

    error page

--- src/Pages/ErrorPage/ErrorPage.tsx
+++ src/Pages/ErrorPage/ErrorPage.tsx
@@ -1,7 +1,16 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
+import "./ErrorPage.scss";
 const ErrorPage: React.FC = () => {
-  return <div>ErrorPage</div>;
+  return (
+    <>
+      <div className="error-container">
+        <h1>404 Page Not Found</h1>
+        <p>The page you are looking for does not exist.</p>
+        <Link to="/">Back to Home</Link>
+      </div>
+    </>
+  );
 };
 
 export default ErrorPage;
